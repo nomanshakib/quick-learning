c16ef20d804a455e7ca0bb194b034dcd9f476908
nomanshakib <nomanshakib.official@gmail.com>
2022-11-05 20:54:46 +0600

    firebase connection

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJQN1-XNva999EHWKxjbvhZ7K-o16fbbk",
+  authDomain: "quick-learning-332ae.firebaseapp.com",
+  projectId: "quick-learning-332ae",
+  storageBucket: "quick-learning-332ae.appspot.com",
+  messagingSenderId: "459883955458",
+  appId: "1:459883955458:web:fbc8c634fb9ea3124eee1b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
